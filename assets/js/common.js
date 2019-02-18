bcf782c0410d24642f0105dd54384ecb0398887b
LEEHYUNS00 <47455760+LEEHYUNS00@users.noreply.github.com>
2019-02-18 13:12:18 +0900

    Add files via upload

---
+++ assets/js/common.js
@@ -0,0 +1,40 @@
+(function($) {
+	"use strict"; // Start of use strict
+
+	// Smooth scrolling using jQuery easing
+	$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: (target.offset().top - 72)
+				}, 1000, "easeInOutExpo");
+				return false;
+			}
+		}
+	});
+
+	// Closes responsive menu when a scroll trigger link is clicked
+	$('.js-scroll-trigger').click(function() {
+		$('.navbar-collapse').collapse('hide');
+	});
+	
+	// Header fixed and Back to top button
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('.back-to-top').fadeIn('slow');
+			$('#header').addClass('header-fixed');
+		} else {
+			$('.back-to-top').fadeOut('slow');
+			$('#header').removeClass('header-fixed');
+		}
+	});
+	
+	$('.back-to-top').click(function () {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 1500, 'easeInOutExpo');
+		return false;
+	});
+})(jQuery); // End of use strict
